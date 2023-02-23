6fc69da0bab994d35e0336b8effd4c5023a9debe
bukasj <chukwuebukaedej@gmail.com>
2023-02-22 19:51:54 -0800

    sorts a doubly linked list of integers in ascending order using the Insertion sort algorithm

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -5,6 +5,7 @@
  * in ascending order using the Insertion sort algorithm
  * @list: pointer to the head of the doubly linked list
  */
+
 void insertion_sort_list(listint_t **list)
 {
 	if (list == NULL || *list == NULL || (*list)->next == NULL)
@@ -40,4 +41,29 @@ void insertion_sort_list(listint_t **list)
 
 		curr = curr->next;
 	}
+}
+
+/**
+ *swap_node - swap a node for his previous one
+ *@node: node
+ *@list: node list
+ *Return: return a pointer to a node which was enter it
+ */
+
+listint_t *swap_node(listint_t *node, listint_t **list)
+{
+	listint_t *back = node->prev, *current = node;
+	/*NULL, 19, 48, 9, 71, 13, NULL*/
+
+	back->next = current->next;
+	if (current->next)
+		current->next->prev = back;
+	current->next = back;
+	current->prev = back->prev;
+	back->prev = current;
+	if (current->prev)
+		current->prev->next = current;
+	else
+		*list = current;
+	return (current);
 }
